320f7ae4339eeebe84773abfc500340d21313925
SvenGege <su.moonfy@gmail.com>
2018-08-31 20:45:55 +0200

    Update script.js

--- simple/script.js
+++ simple/script.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
 	var QRBox	=	$('#QRBox');
 	var MainBox	=	$('#MainBox');
-	var AliPayQR	=	'images/AliPayQR.png';
-	var WeChanQR	=	'images/WeChanQR.png';
+	var AliPayQR	=	'images/AlipayQR.png';
+	var WeChanQR	=	'images/WeChatQR.png';
 
 	
 
